dfc6df716ca2ff408bd9cb8a91b1ab0c305bfeed
jy2694 <wpdus2694@naver.com>
2023-01-03 20:47:26 +0900

    spring initial commit

---
+++ src/main/java/kr/mos1981/mosweb/MoswebApplication.java
@@ -0,0 +1,13 @@
+package kr.mos1981.mosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/mos1981/mosweb/MoswebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mos1981.mosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
